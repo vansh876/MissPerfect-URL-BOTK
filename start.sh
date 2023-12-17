7fedccddeb5ef1184b36bdcbf4eb0cf8d3c94344
vansh876 <137757076+vansh876@users.noreply.github.com>
2023-12-17 10:07:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 echo "Cloning Repo...."
-git clone https://github.com/LazyDeveloperr/MissPerfect-URL-BOT /MissPerfectURLBOT
-cd /MissPerfectURLBOT
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
